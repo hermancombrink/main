52e2624ce8c4ab3b173ffd2f457b06251d609ac3
herman.combrink@gmail.com <herman.combrink@gmail.com>
2014-08-17 12:47:50 +0200

    .

--- websites/alieziaherman.co.za/Scripts/site.js
+++ websites/alieziaherman.co.za/Scripts/site.js
@@ -10,15 +10,17 @@
         // Components							
         slide_links: 'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
         slides: [			// Slideshow Images
-                                            { image: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/kazvan-1.jpg', title: 'Image Credit: Maria Kazvan', thumb: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/kazvan-1.jpg', url: '' },
-                                            { image: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/kazvan-2.jpg', title: 'Image Credit: Maria Kazvan', thumb: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/kazvan-2.jpg', url: '' },
-                                            { image: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/kazvan-3.jpg', title: 'Image Credit: Maria Kazvan', thumb: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/kazvan-3.jpg', url: '' },
-                                            { image: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/wojno-1.jpg', title: 'Image Credit: Colin Wojno', thumb: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/wojno-1.jpg', url: '' },
-                                            { image: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/wojno-2.jpg', title: 'Image Credit: Colin Wojno', thumb: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/wojno-2.jpg', url: '' },
-                                            { image: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/wojno-3.jpg', title: 'Image Credit: Colin Wojno', thumb: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/wojno-3.jpg', url: '' },
-                                            { image: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/shaden-1.jpg', title: 'Image Credit: Brooke Shaden', thumb: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/shaden-1.jpg', url: '' },
-                                            { image: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/shaden-2.jpg', title: 'Image Credit: Brooke Shaden', thumb: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/shaden-2.jpg', url: '' },
-                                            { image: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/slides/shaden-3.jpg', title: 'Image Credit: Brooke Shaden', thumb: 'http://buildinternet.s3.amazonaws.com/projects/supersized/3.2/thumbs/shaden-3.jpg', url: '' }
+                                            { image: 'Images/1.jpg', title: 'Come Fly With Us! Please Join Us As We Take The Trip Of A Lifetime...', thumb: '', url: '' },
+                                             { image: 'Images/2.jpg', title: 'Come Fly With Us! Please Join Us As We Take The Trip Of A Lifetime...', thumb: '', url: '' },
+                                              { image: 'Images/3.jpg', title: 'Come Fly With Us! Please Join Us As We Take The Trip Of A Lifetime...', thumb: '', url: '' },
+                                               { image: 'Images/4.jpg', title: 'Come Fly With Us! Please Join Us As We Take The Trip Of A Lifetime...', thumb: '', url: '' },
+                                                { image: 'Images/5.jpg', title: 'Come Fly With Us! Please Join Us As We Take The Trip Of A Lifetime...', thumb: '', url: '' },
+                                                 { image: 'Images/6.jpg', title: 'Come Fly With Us! Please Join Us As We Take The Trip Of A Lifetime...', thumb: '', url: '' },
+                                                  { image: 'Images/7.jpg', title: 'Come Fly With Us! Please Join Us As We Take The Trip Of A Lifetime...', thumb: '', url: '' },
+                                                   { image: 'Images/8.jpg', title: 'Come Fly With Us! Please Join Us As We Take The Trip Of A Lifetime...', thumb: '', url: '' },
+                                                    { image: 'Images/9.jpg', title: 'Come Fly With Us! Please Join Us As We Take The Trip Of A Lifetime...', thumb: '', url: '' },
+                                                     { image: 'Images/10.jpg', title: 'Come Fly With Us! Please Join Us As We Take The Trip Of A Lifetime...', thumb: '', url: '' }
+                                          
         ]
 
     });
