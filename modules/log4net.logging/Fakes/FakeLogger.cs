3014f95ab9e1cb6acdb25f4c9a0b81674e375e0d
herman.combrink@gmail.com <herman.combrink@gmail.com>
2014-08-19 22:33:30 +0200

    .

---
+++ modules/log4net.logging/Fakes/FakeLogger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace log4net.logging.Fakes
+{
+    public class FakeLogger : ILogger
+    {
+        public ILog Log
+        {
+            get { return new LogFake(); }
+        }
+
+        public void SafeException(Exception ex, string msg, string TAG, LogLevel level = LogLevel.FATAL)
+        {
+            return;
+        }
+
+        public Exception GetSafeException(Exception ex, string msg, string TAG, LogLevel level = LogLevel.FATAL)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RaiseException(string msg, string TAG, LogLevel level = LogLevel.ERROR)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Exception GetRaiseException(string msg, string TAG, LogLevel level = LogLevel.ERROR)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Debug(string msg, string TAG, LogLevel level = LogLevel.DEBUG)
+        {
+            return;
+        }
+
+        public void Info(string msg, string TAG, LogLevel level = LogLevel.INFO)
+        {
+            return;
+        }
+
+        public void Warn(string msg, string TAG, LogLevel level = LogLevel.WARN)
+        {
+            return;
+        }
+    }
+}
---
+++ tests/MailerTests/MailTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using log4net.logging;
+using log4net.logging.Fakes;
+using send.mail;
+
+namespace MailerTests
+{
+    [TestClass]
+    public class MailTests
+    {
+        ILogger logger = new FakeLogger();
+
+        [TestMethod]
+        public void TestConfigureMail()
+        {
+            Mailer mailer = new Mailer(logger);
+        }
+    }
+}
